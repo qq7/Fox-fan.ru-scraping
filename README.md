ab0d0b417da3c71f6d98c2c0f493b721c54bd07b
qq7 <hi@420blaze.it>
2016-11-13 19:47:23 +0200

    Now it parses all the episodes available. Edited README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fox-fan.ru-scraping
+A tiny script made to get a list of every South Park epiode available on southpark.cc-fan.ru
+Returns unreusable direct links to mp4 files
--- crawler.py
+++ crawler.py
@@ -3,23 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_seasons(season):
+def get_seasons():
     episodes = []
-    for episode in range(1, 15):
-        voice_original = '&voice=3'
-        if episode <= 9:
-            url = 'http://southpark.cc-fan.ru/series.php?id=' + str(season) + '0' + str(episode) + voice_original
-        else:
-            url = 'http://southpark.cc-fan.ru/series.php?id=' + str(season) + str(episode) + voice_original
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text)
-        link = soup.findAll('script')[19].string
-        file = re.search(r'http:/\/m.+mp4', link)
-        # return file.group(0)
-        episodes.append(file.group(0))
-    print(episodes)
-    return episodes
+    for season in range(1, 21):
+        try:
+            for episode in range(1, 15):
+                voice_original = '&voice=3'
+                if episode <= 9:
+                    url = 'http://southpark.cc-fan.ru/series.php?id=' + str(season) + '0' + str(episode) + voice_original
+                else:
+                    url = 'http://southpark.cc-fan.ru/series.php?id=' + str(season) + str(episode) + voice_original
+                source_code = requests.get(url)
+                plain_text = source_code.text
+                soup = BeautifulSoup(plain_text)
+                link = soup.findAll('script')[19].string
+                file = re.search(r'http:\/\/m.+mp4', link)
+                episodes.append(file.group(0))
 
-x = str(input('Enter season: '))
-get_seasons(x)
+        except IndexError:
+            pass
+    print(episodes)
+    
+get_seasons()
+
